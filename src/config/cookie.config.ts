8ba67a985fd7eeb63b0afca103720053ffecfbb9
tkdgh7063 <hoo412298@gmail.com>
2026-05-03 21:20:51 +0900

    fix: 쿠키 옵션 설정 수정

--- src/config/cookie.config.ts
+++ src/config/cookie.config.ts
@@ -1,7 +1,7 @@
 const config = {
   httpOnly: true,
   secure: process.env.NODE_ENV === 'production',
-  sameSite: 'none',
+  sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
 } as const;
 
 export default config;
