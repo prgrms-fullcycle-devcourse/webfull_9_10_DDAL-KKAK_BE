1c65fb5b4ad8614f9afc56706eb255840f2747b1
tkdgh7063 <hoo412298@gmail.com>
2026-05-03 19:18:40 +0900

    feat: 로그아웃 API

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,17 @@
 import { Router } from 'express';
 
-import { finishLogin, startLogin } from '../controllers/auth.controller.js';
+import {
+  finishLogin,
+  logoutUser,
+  startLogin,
+} from '../controllers/auth.controller.js';
+import { authenticate } from '../middlewares/auth.middleware.js';
 
 const router = Router({ mergeParams: true });
 
 router.get('/:provider/login', startLogin);
 router.get('/:provider/callback', finishLogin);
+
+router.post('/logout', authenticate, logoutUser);
 
 export default router;
