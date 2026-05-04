823458c96fd4fe484172d2a86b283b0c244bbbc6
tkdgh7063 <hoo412298@gmail.com>
2026-05-04 13:41:17 +0900

    Revert "feat: trips 상세 조회, 수정, 삭제 API 구현 (#12)"
    This reverts commit 50d0016774e5082869572d235d028932366e56a4.

--- src/constants/crypto.ts
+++
@@ -1,3 +0,0 @@
-export const CRYPTO_CONSTANTS = {
-  SALT_ROUNDS: 10,
-} as const;
--- src/routes/currencies.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { StatusCodes } from 'http-status-codes';
-
-const router = Router();
-
-router.get('/', (_req, res) => res.status(StatusCodes.OK).send('Currencies'));
-
-export default router;
