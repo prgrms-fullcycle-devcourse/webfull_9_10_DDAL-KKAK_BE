c054e1f424564ce012e53962d0c23e573fdb8ed8
tkdgh7063 <hoo412298@gmail.com>
2026-05-05 18:05:14 +0900

    fix: state 값 검증 로직 수정

--- src/config/cookie.config.ts
+++ src/config/cookie.config.ts
@@ -1,7 +1,10 @@
 const config = {
-  httpOnly: true,
-  secure: process.env.NODE_ENV === 'production',
-  sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+  option: {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+  },
+  secret: process.env.SESSION_SECRET as string,
 } as const;
 
 export default config;
