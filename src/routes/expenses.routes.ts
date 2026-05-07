889e4ac4882e6135861fbe9886d89cdd4a6d9cb3
jihwan <jihwan0713@naver.com>
2026-05-07 20:57:41 +0900

    Refactor: OCR

--- src/routes/expenses.routes.ts
+++ src/routes/expenses.routes.ts
@@ -2,14 +2,18 @@ import { Router } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 import {
+  createExpense,
   createReceiptOcrJob,
   deleteReceiptOcrJob,
   getReceiptOcrJob,
+  updateExpense,
 } from '../controllers/expenses.controller.js';
 
 const router = Router();
 
 router.get('/', (_req, res) => res.status(StatusCodes.OK).send('Expenses'));
+router.post('/', createExpense);
+router.patch('/:expenseId', updateExpense);
 router.post('/ocr', createReceiptOcrJob);
 router.get('/ocr/:receiptId', getReceiptOcrJob);
 router.delete('/ocr/:receiptId', deleteReceiptOcrJob);
